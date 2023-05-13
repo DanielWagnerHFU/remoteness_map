7d943593860246f0c4062b78baca3ef70f0051ad
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2023-05-13 18:10:47 +0200

    Update main.py

--- RemotenessMap/main.py
+++ RemotenessMap/main.py
@@ -41,16 +41,12 @@ def calculate_minimum_distance(point, gdf):
     return min_distance
 
 def generate_distance_plot(gdf, resolution=0.0003):
-    # Determine the bounding box of the GeoDataFrame
     bounds = gdf.total_bounds
     minx, miny, maxx, maxy = bounds
-
-    # Create a grid of points within the bounding box with specified resolution
     x = np.arange(minx, maxx, resolution)
     y = np.arange(miny, maxy, resolution)
     xx, yy = np.meshgrid(x, y)
 
-    # Calculate the distance from each grid point to the nearest road
     distances = np.zeros_like(xx)
     for i in range(xx.shape[0]):
         for j in range(xx.shape[1]):
@@ -58,13 +54,9 @@ def generate_distance_plot(gdf, resolution=0.0003):
             nearest_line = nearest_points(query_point, gdf.unary_union)[1]
             distances[i, j] = query_point.distance(nearest_line)
 
-    # Plot the distances using imshow
     plt.imshow(distances, cmap='ocean_r', extent=[minx, maxx, miny, maxy], origin='lower')
     plt.colorbar(label='Distance (meters)')
-
-    # Plot the roads from the GeoDataFrame on top of the distance plot
     gdf.plot(ax=plt.gca(), color='black', linewidth=1)
-
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.title('Distance from Roads')
