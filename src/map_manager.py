be202036f829d18da5e50c2cc20f2b8793a02771
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2023-12-11 19:16:00 +0100

    updated

--- src/map_manager.py
+++ src/map_manager.py
@@ -3,15 +3,22 @@ import json
 from typing import Any, Dict, List
 from map_source import MapSource
 from map_info import MapInfo
+from map import Map
 
 class MapManager:
     def __init__(self, map_types_json: Dict[str, Any]):
-        if map_types_json["info"]["version"] == "0":
-            self.map_types_json: Dict[str, Any] = map_types_json
-        else:
+        if map_types_json["info"]["version"] != "0":
             raise ValueError(f"Unhandled json version")
+        
+        self.map_types_json: Dict[str, Any] = map_types_json
+        self.map_sources: Dict[str, MapSource] = {}
 
-    def build_map(self, map_info: MapInfo):
+    def get_map(self, map_info: MapInfo) -> None:
+        map_source: MapSource = self.map_sources[map_info.map_type]
+        map: Map = map_source.get_map(map_info)
+        print(map)
+
+    def build_map(self, map_info: MapInfo) -> None:
         map_type_json: Dict[str, Any] = self._get_map_type_json(map_info.map_type)
         #TODO foreach map build map source - maype outside of this function in initialization
         map_source: MapSource = MapSource(map_type_json["source"])
@@ -27,11 +34,11 @@ class MapManager:
         map_type_json: Dict[str, Any] = list(filter(lambda x: x["map_type"] == "all_ways", types_list_json))[0]
         return map_type_json
     
-    def initialize_map_sources(self):
-        types_json: Dict[str, Any] = self.map_types_json["types"]
-        for type in types_json:
-            print(type)
-
+    def initialize_map_sources(self) -> None:
+        types_json: List[Dict[str, Any]] = self.map_types_json["types"]
+        for type_json in types_json:
+            map_source: MapSource = MapSource(type_json["source"])
+            self.map_sources[type_json["map_type"]] = map_source
 
 if __name__ == "__main__":
     map_types_path: Path = Path(R"C:\Users\danie\Documents\Git\remoteness_map\data\map_types\map_types.json")
--- src/overpass_source_implementation.py
+++ src/overpass_source_implementation.py
@@ -2,12 +2,17 @@ from map_source_implementation import MapSourceImplementation
 from map import Map
 from typing import Any, Dict
 from map_info import MapInfo
+from overpass_api_client import OverpassAPIClient
 
 class OverpassSourceImplementation(MapSourceImplementation):
+
+
     def __init__(self, source_json: Dict[str, Any]):
-        #TODO
-        pass
+        self.overpass_api_client = OverpassAPIClient(source_json["api_endpoint"])
+        self.query_scheme = source_json["query_scheme"]
 
     def get_map(self, map_info: MapInfo) -> Map:
         #TODO
+        formatted_query: str = eval(f'f"""{self.query_scheme}"""')
+        print(formatted_query)
         return Map()
