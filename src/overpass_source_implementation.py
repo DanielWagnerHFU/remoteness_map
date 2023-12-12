1c7a05f4cf0ed85d12d9ad05bf632a53fc1bc0f8
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2023-12-12 14:30:19 +0100

    Update overpass_source_implementation.py

--- src/overpass_source_implementation.py
+++ src/overpass_source_implementation.py
@@ -3,6 +3,7 @@ from map import Map
 from typing import Any, Dict
 from map_info import MapInfo
 from overpass_api_client import OverpassAPIClient
+import json
 
 class OverpassSourceImplementation(MapSourceImplementation):
 
@@ -14,4 +15,5 @@ class OverpassSourceImplementation(MapSourceImplementation):
     def get_map(self, map_info: MapInfo) -> Map:
         formatted_query: str = self.query_scheme.format(radius = map_info.height_meter, lat = map_info.latitude, long = map_info.longitude)
         map_data: str = self.overpass_api_client.execute_query(formatted_query)
-        return Map(map_data)
+        map_json = json.loads(map_data)
+        return Map()
