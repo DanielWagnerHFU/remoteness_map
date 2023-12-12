0549942b452ffe608607f5327b2b390d2ede6333
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2023-12-12 14:30:16 +0100

    Update map.py

--- src/map.py
+++ src/map.py
@@ -2,6 +2,5 @@ import json
 
 
 class Map:
-    def __init__(self, map_data: str):
-        map_json = json.loads(map_data)
-        print(map_json)
+    def __init__(self):
+        print("map created")
