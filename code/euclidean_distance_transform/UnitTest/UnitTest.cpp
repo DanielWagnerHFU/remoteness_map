299f2e1d0b231f6aeba1547a0bfc034173960a06
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2024-05-21 17:43:03 +0200

    Update UnitTest.cpp

--- code/euclidean_distance_transform/UnitTest/UnitTest.cpp
+++ code/euclidean_distance_transform/UnitTest/UnitTest.cpp
@@ -1,5 +1,11 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+#include "DistanceTransformContext.hpp"
+#include "CV2EuclideanDistanceTransformStrategy.hpp"
+#include "DistanceTransformStrategy.hpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -8,9 +14,10 @@ namespace UnitTest
 	TEST_CLASS(UnitTest)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
+
 		}
 	};
 }
