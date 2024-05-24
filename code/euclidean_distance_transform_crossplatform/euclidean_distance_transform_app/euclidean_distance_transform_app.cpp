107cb6873605e0d2bd79d41088883537cc77f1b0
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2024-05-24 20:09:01 +0200

    Added CMake Project with dummy files

---
+++ code/euclidean_distance_transform_crossplatform/euclidean_distance_transform_app/euclidean_distance_transform_app.cpp
@@ -0,0 +1,13 @@
+﻿// euclidean_distance_transform_app.cpp : Defines the entry point for the application.
+//
+
+#include "euclidean_distance_transform_app.h"
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
